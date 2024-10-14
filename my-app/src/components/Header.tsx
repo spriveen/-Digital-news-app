71a7f4987ab04ffafaf95316698efd623c62b072
spriveen <rpeanuka@gmail.com>
2024-10-14 09:45:01 -0700

    update tooglemenu

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,26 +1,55 @@
 'use client';
 
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './header.css';
 import Nav from './Nav';
 import Sci from './Sci';
-
+import SearchForm from './SearchForm';
 
 export default function Header() {
-  return(
-    <header id='header' 
-    className='header d-flex algin-items-center fixed-top'
-    >
-   <div className='container-flud container-xl d-flex algin-items-center justify-content-between'>
-    <a href='/' className='logo d-flex algin-items-center'>
-    {/* <img src='' alt='' */}
-    <h1>Digital News</h1>
-    </a>
-    <Nav />
-    <div className='position-relative'> 
-      <Sci />
-    </div>
-    </div>
+  const [open, setOpen] = useState(false);
+  const [on, setOn] = useState(false);
+
+  const handleFormOpen = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    setOpen(!open);
+  };
+
+  const handleToggleMenu = () => {
+    setOn(!on);
+    const body: HTMLElement | null = document.querySelector('body');
+    if (body) {
+      body.classList.toggle('mobile-nav-active');
+    }
+  };
+
+  return (
+    <header id="header" className="header d-flex align-items-center fixed-top">
+      <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
+        <a href="/" className="logo d-flex align-items-center">
+          {/* <img src='' alt='' /> */}
+          <h1>Digital News</h1>
+        </a>
+        <Nav />
+        <div className="position-relative">
+          <Sci />
+          <a className="mx-2 js-search-open" onClick={handleFormOpen}>
+            <span className="bi-search"></span>
+          </a>
+          {on ? (
+            <i
+              className="bi bi-x mobile-nav-toggle"
+              onClick={handleToggleMenu}
+            ></i>
+          ) : (
+            <i
+              className="bi bi-list mobile-nav-toggle"
+              onClick={handleToggleMenu}
+            ></i>
+          )}
+          <SearchForm active={open} formOpen={handleFormOpen} />
+        </div>
+      </div>
     </header>
-  )
-  }
+  );
+}
